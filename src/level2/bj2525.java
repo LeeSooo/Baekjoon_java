8e98450b7eaba0b67cd75c7dfb10fa00a3a3cde0
LeeSooo <dltn990930@naver.com>
2022-07-26 11:44:46 +0900

    2단계 문제

---
+++ src/level2/bj2525.java
@@ -0,0 +1,34 @@
+package level2;
+// 오븐 시계
+/* 
+ * 알고리즘 접근 방법
+ * 1. A(시)와 B(분)을 하나의 분으로 변환한 뒤, C를 더하고 나온 결과 값을 다시 시와 분 으로 변환하는 방법
+ * 2. C를 시와 분으로 변환한 뒤, 각 나뉘어진 시와 분을 A와 B에 각각 더하여 연산하는 방법
+ */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+ 
+public class bj2525 {
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+ 
+        int C = Integer.parseInt(br.readLine());
+ 
+        int min = 60 * A + B;   // 시 -> 분
+        min += C;
+ 
+        int hour = (min / 60) % 24;
+        int minute = min % 60;
+ 
+        System.out.println(hour + " " + minute);
+ 
+    }
+}
--- src/level2/bj2884.java
+++ src/level2/bj2884.java
@@ -1,4 +1,18 @@
 package level2;
+// 알람시계
+/*
+ * ※ 주의할 점
+ * - 24시간 표현을 사용한다
+ * - 입력은 공백 한 칸을 기준으로 구분되어 입력된다.
+ * - 출력시 불필요한 0은 사용하지 않는다.
+ */
+
+/*
+ * ※ 알고리즘 접근 방법
+ * 1-1 단계 : 45분보다 작은지 여부 - if (min < 45 ) 
+ * 2-1 단계 : 시(hour) 가 0보다 작은지 여부 - if (hour < 0 )
+ * 1-2 단계 : 1단계 조건 외 여부 - else
+ */
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
