73d25da0176474aa5cadb78b036befe9ae07f1aa
LeeSooo <dltn990930@naver.com>
2022-07-26 11:23:37 +0900

    내용 수정

--- src/level1/bj10718.java
+++ src/level1/bj10718.java
@@ -7,13 +7,34 @@ import java.io.IOException;
  
 public class bj10718 {
 	public static void main(String[] args) throws IOException {
+		/* 
+		 * 방법 1. (System 클래스 - 표준 입출력)
+		 * System.out.println("강한친구 대한육군");
+		 * System.out.println("강한친구 대한육군");    
+		 */
+		
+		// 방법 2. BufferedWriter 를 이용하는 방법 *(중요)*
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
- 
 		bw.write("강한친구 대한육군");
-		bw.newLine();	// 줄 구분자
+		bw.newLine();	// '\n'과 같은 '역할'을 수행하지만, '\n'과 같은 것은 아님.  
 		bw.write("강한친구 대한육군");
-        
 		bw.flush();
 		bw.close();
+		
+		/* 
+		 * 방법 3. StringBuilder 를 이용하는 방법
+		 * StringBuilder sb = new StringBuilder();		
+		 * sb.append("강한친구 대한육군\n");		
+		 * sb.append("강한친구 대한육군");		
+		 * System.out.println(sb);
+		 */
+		
+		/*
+		 * 방법 4. StringBuffer 를 이용하는 방법
+		 * StringBuffer sb = new StringBuffer();
+		 * sb.append("강한친구 대한육군\n");		
+		 * sb.append("강한친구 대한육군");		
+		 * System.out.println(sb);
+		 */
 	}
 }
--- src/level1/bj2588.java
+++ src/level1/bj2588.java
@@ -1,5 +1,6 @@
 package level1;
 
+// 곱셈
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,25 +8,59 @@ import java.io.InputStreamReader;
 public class bj2588 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 
+
 		int A = Integer.parseInt(br.readLine());
 		int B = Integer.parseInt(br.readLine());
-		
+
 		br.close();
-        
+
 		StringBuilder sb = new StringBuilder();
-        
-		sb.append(A * (B%10));
+
+		sb.append(A * (B % 10));
 		sb.append('\n');
-		
-		sb.append(A * ((B%100)/10));
+
+		sb.append(A * ((B % 100) / 10));
 		sb.append('\n');
-        
-		sb.append(A * (B/100));
+
+		sb.append(A * (B / 100));
 		sb.append('\n');
-        
+
 		sb.append(A * B);
- 
+
 		System.out.print(sb);
+
+		/*
+		 * 1번 방법 
+		 * Scanner in = new Scanner(System.in); 
+		 * int A = in.nextInt(); 
+		 * String B = in.next(); 
+		 * in.close(); 
+		 * System.out.println(A * (B.charAt(2) - '0'));
+		 * System.out.println(A * (B.charAt(1) - '0')); 
+		 * System.out.println(A * (B.charAt(0) - '0')); 
+		 * System.out.println(A * Integer.parseInt(B));
+		 */
+
+		/*
+		 * 2번 방법 Scanner in = new Scanner(System.in);
+		 * int A = in.nextInt(); int B = in.nextInt();
+		 * in.close();
+		 * System.out.println(A*(B%10)); 
+		 * System.out.println(A*(B%100/10));
+		 * System.out.println(A*(B/100)); 
+		 * System.out.println(A*B);
+		 */
+
+		/*
+		 * 3번 방법 BufferedReader br = new BufferedReader(new
+		 * InputStreamReader(System.in));
+		 * int A = Integer.parseInt(br.readLine()); 
+		 * String B = br.readLine();
+		 * char[] b = B.toCharArray();
+		 * System.out.println(A * (b[2]-'0')); 
+		 * System.out.println(A * (b[1]-'0'));
+		 * System.out.println(A * (b[0]-'0')); 
+		 * System.out.println(A * Integer.parseInt(B));
+		 */
 	}
 }
