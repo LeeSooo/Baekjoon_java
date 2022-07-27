6e400a4e7d974095be53ff5f0c63f5b5206dc279
LeeSooo <dltn990930@naver.com>
2022-07-27 11:44:37 +0900

    3단계 3번 문제 합

---
+++ src/level3/bj8393.java
@@ -0,0 +1,18 @@
+package level3;
+
+// n이 주어졌을 때, 1부터 n까지 합을 구하는 프로그램을 작성하시오.
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+ 
+public class bj8393 {
+	public static void main(String[] args) throws IOException {
+		int n = Integer.parseInt(new BufferedReader(new InputStreamReader(System.in)).readLine());
+		int s = 0;
+        
+		for(int i = 1 ; i <= n ; i++ ) s+=i;
+ 
+		System.out.println(s);
+	}
+}
