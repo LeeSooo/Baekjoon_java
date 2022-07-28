ae4e787eb29b6cf485f7d5c9ada36bfc47144483
LeeSooo <dltn990930@naver.com>
2022-07-28 11:39:36 +0900

    3단계 9번 문제 (별찍기 1)

---
+++ src/level3/bj2438.java
@@ -0,0 +1,26 @@
+package level3;
+
+// 별 찍기 1 문제
+// 첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class bj2438 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		br.close();
+ 
+		StringBuilder sb = new StringBuilder();
+ 
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= i; j++) {
+				sb.append('*');
+			}
+			sb.append('\n');
+		}
+		System.out.print(sb);
+	}
+}
